20b7c492361dfcde8ab42ecbcd22b2193966eaa1
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2023-06-11 21:57:13 -0400

    card control by block property

--- src/components/LinkedProfile.js
+++ src/components/LinkedProfile.js
@@ -5,7 +5,7 @@ import {
     ProfileFilter,
     ProfileSorter,
     useLinkedProfileFilterState,
-    ProfileImage,
+    ProfileImage
 } from '@uniwebcms/module-sdk';
 import MediaHeader from '../ui/MediaHeader';
 
@@ -21,69 +21,46 @@ const Card = ({ profile, properties = {} }) => {
             className={`overflow-hidden group hover:bg-gray-50 rounded-xl px-6 py-4 ${
                 border ? 'border' : ''
             }`}
-            style={style}
-        >
-            <div className="flex justify-between">
-                <div className="w-20 h-20 rounded-full overflow-hidden">
-                    <ProfileImage profile={profile} type="banner" />
+            style={style}>
+            <div className='flex justify-between'>
+                <div className='w-20 h-20 rounded-full overflow-hidden'>
+                    <ProfileImage profile={profile} type='banner' />
                 </div>
             </div>
 
-            <div className="text-xl font-medium text-gray-700 truncate mt-5">
-                {title}
-            </div>
+            <div className='text-xl font-medium text-gray-700 truncate mt-5'>{title}</div>
             {subtitle ? (
-                <div className="text-lg font-normal text-gray-500 truncate mt-2">
-                    {subtitle}
-                </div>
+                <div className='text-lg font-normal text-gray-500 truncate mt-2'>{subtitle}</div>
             ) : null}
         </Link>
     );
 };
 
-const Cards = ({
-    mainProfile,
-    profileType,
-    section,
-    showFilter,
-    hasSorting,
-    cardProperties,
-}) => {
+const Cards = ({ mainProfile, profileType, section, showFilter, hasSorting, cardProperties }) => {
     const [pt, vt = 'profile'] = profileType.split('/');
 
-    const [filter, setFilter] = useLinkedProfileFilterState(
-        mainProfile,
-        `${pt}/${vt}`,
-        section
-    );
+    const [filter, setFilter] = useLinkedProfileFilterState(mainProfile, `${pt}/${vt}`, section);
 
     const { filtered } = filter;
 
     return (
-        <div className="mt-4 sm:mt-8 space-y-14">
-            <div className="flex justify-end">
-                <div className="flex space-x-1 items-center">
+        <div className='mt-4 sm:mt-8 space-y-14'>
+            <div className='flex justify-end'>
+                <div className='flex space-x-1 items-center'>
                     {showFilter ? (
                         <ProfileFilter filter={filter} setFilter={setFilter}>
                             <ProfileFilter.Search />
                             <ProfileFilter.Menu />
                         </ProfileFilter>
                     ) : null}
-                    {hasSorting ? (
-                        <ProfileSorter filter={filter} setFilter={setFilter} />
-                    ) : null}
+                    {hasSorting ? <ProfileSorter filter={filter} setFilter={setFilter} /> : null}
                 </div>
             </div>
             <ul
-                role="list"
-                className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
-            >
+                role='list'
+                className='grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3'>
                 {filtered.map((profile) => (
-                    <Card
-                        key={profile.key}
-                        profile={profile}
-                        properties={cardProperties}
-                    />
+                    <Card key={profile.key} profile={profile} properties={cardProperties} />
                 ))}
             </ul>
         </div>
@@ -97,17 +74,12 @@ export default function (props) {
         section,
         filter: showFilter = true,
         sorting: hasSorting = true,
-        card,
+        card
     } = block.getBlockProperties();
 
     return (
         <>
-            <MediaHeader
-                page={page}
-                website={website}
-                height="600px"
-                width="100%"
-            />
+            <MediaHeader page={page} website={website} />
             <Container>
                 {profileType && section ? (
                     <Cards
--- src/ui/MediaHeader.js
+++ src/ui/MediaHeader.js
@@ -18,12 +18,10 @@ const MediaBackground = ({ src, alt }) => {
             return (
                 <img
                     src={src}
-                    className={
-                        'absolute top-0 left-0 w-full h-full object-cover'
-                    }
+                    className={'absolute top-0 left-0 w-full h-full object-cover'}
                     style={{ zIndex: -1 }}
                     alt={alt}
-                    loading="lazy"
+                    loading='lazy'
                 />
             );
         } else if (mediaType === 'video') {
@@ -32,11 +30,8 @@ const MediaBackground = ({ src, alt }) => {
                     autoPlay
                     muted
                     loop
-                    className={
-                        'absolute top-0 left-0 w-full h-full object-cover'
-                    }
-                    style={{ zIndex: -1 }}
-                >
+                    className={'absolute top-0 left-0 w-full h-full object-cover'}
+                    style={{ zIndex: -1 }}>
                     <source src={src} type={`video/${ext}`}></source>
                     {alt || 'Your browser does not support the video tag.'}
                 </video>
@@ -46,55 +41,46 @@ const MediaBackground = ({ src, alt }) => {
 };
 
 export default function MediaHeader(props) {
-    const {
-        page,
-        website,
-        width = '',
-        height = '',
-        // mediaClassName,
-        // mediaStyle,
-    } = props;
+    const { page, website, width = '100%', height = '600px' } = props;
+
+    const webProfile = website.getWebsiteProfile();
 
     const pageTitle = page.getPageTitle();
     const pageLeadText = page.getPageLeadText();
+    const pageHeaderImg = page.getPageHeaderImg();
 
-    const webProfile = website.getWebsiteProfile();
-    const { src, altText } = webProfile.getAssetInfo(page.getPageHeaderImg());
+    let src, alt;
+
+    if (pageHeaderImg) {
+        ({ src, altText: alt } = webProfile.getAssetInfo(pageHeaderImg));
+    }
 
     return (
-        <div className="relative" style={{ height, width }}>
+        <div className='relative' style={{ height, width }}>
             <div
-                className={`h-full z-1 relative flex flex-col justify-center px-4 sm:px-8 md:px-12 lg:px-20`}
-            >
+                className={`h-full z-1 relative flex flex-col justify-center px-4 sm:px-8 md:px-12 lg:px-20`}>
                 {pageTitle ? (
                     <p
-                        className="text-4xl md:text-5xl font-bold text-white"
+                        className='text-4xl md:text-5xl font-bold text-white'
                         style={{
                             wordSpacing: '4px',
-                            textShadow: 'black 1px 0 10px',
-                        }}
-                    >
+                            textShadow: 'black 1px 0 10px'
+                        }}>
                         {pageTitle}
                     </p>
                 ) : null}
                 {pageLeadText ? (
                     <p
-                        className="mt-4 text-lg md:text-xl text-gray-100"
+                        className='mt-4 text-lg md:text-xl text-gray-100'
                         style={{
                             wordSpacing: '4px',
-                            textShadow: 'black 1px 0 10px',
-                        }}
-                    >
+                            textShadow: 'black 1px 0 10px'
+                        }}>
                         {pageLeadText}
                     </p>
                 ) : null}
             </div>
-            <MediaBackground
-                src={src}
-                alt={altText}
-                // className={mediaClassName}
-                // style={mediaStyle}
-            />
+            {src ? <MediaBackground src={src} alt={alt} /> : null}
         </div>
     );
 }
