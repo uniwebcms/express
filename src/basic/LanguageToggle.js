137b24056adb73aeb2fa969c2637b3c6f369b5af
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2023-06-29 16:21:39 -0400

    fix

--- src/basic/LanguageToggle.js
+++ src/basic/LanguageToggle.js
@@ -26,42 +26,38 @@ export default function (props) {
     const currentLang = website.getLanguage();
     const langPreference = website.getLanguagePreference();
 
-    let langOptions = [];
-
     if (langPreference === 'bilingual') {
-        langOptions = [
+        let langOptions = [
             { label: website.localize({ en: 'English', fr: 'Anglais' }), value: 'en' },
             { label: website.localize({ en: 'French', fr: 'Français' }), value: 'fr' }
         ];
-    } else if (langPreference === 'en') {
-        langOptions = [{ label: website.localize({ en: 'English', fr: 'Anglais' }), value: 'en' }];
-    } else if (langPreference === 'fr') {
-        langOptions = [{ label: website.localize({ en: 'French', fr: 'Français' }), value: 'fr' }];
+
+        const menu = langOptions.map((opt) => (
+            <div
+                key={opt.value}
+                className={twJoin(
+                    'w-full px-3 py-1.5 text-lg',
+                    opt.value === currentLang
+                        ? 'cursor-not-allowed text-gray-400'
+                        : 'cursor-pointer hover:bg-gray-50 hover:text-blue-500'
+                )}
+                onClick={() => {
+                    if (opt.value !== currentLang) website.changeLanguage(opt.value);
+                }}>
+                {opt.label}
+            </div>
+        ));
+
+        return (
+            <PopoverMenu
+                trigger={<MdLanguage className='w-full h-full' />}
+                triggerClassName='w-6 h-6 flex items-center justify-center text-gray-500 hover:text-gray-700'
+                options={menu}
+                width={'100px'}
+                position={'top-full right-0 mt-1'}
+            />
+        );
     }
 
-    const menu = langOptions.map((opt) => (
-        <div
-            key={opt.value}
-            className={twJoin(
-                'w-full px-3 py-1.5 text-lg',
-                opt.value === currentLang
-                    ? 'cursor-not-allowed text-gray-400'
-                    : 'cursor-pointer hover:bg-gray-50 hover:text-blue-500'
-            )}
-            onClick={() => {
-                if (opt.value !== currentLang) website.changeLanguage(opt.value);
-            }}>
-            {opt.label}
-        </div>
-    ));
-
-    return (
-        <PopoverMenu
-            trigger={<MdLanguage className='w-full h-full' />}
-            triggerClassName='w-6 h-6 flex items-center justify-center text-gray-500 hover:text-gray-700'
-            options={menu}
-            width={'100px'}
-            position={'top-full right-0 mt-1'}
-        />
-    );
+    return null;
 }
