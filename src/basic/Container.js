c591a612470d1f663ec12f3ae7d6197e9fb61220
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2023-07-10 17:13:32 -0400

    Container can now received React Component as props 'as'

--- src/basic/Container.js
+++ src/basic/Container.js
@@ -18,16 +18,21 @@ import { twMerge } from '@uniwebcms/module-sdk';
  * }
  *
  * @component Container
+ * @prop {string|React.Component} as - The HTML element type or React component to use as the container.
  * @prop {string} className - Additional tailwind class names.
  * @prop {ReactNode|ReactNodeArray} children - The contents for the Badge container.
  * @returns {function} A react component.
  */
-export default function ({ as: Component = 'section', children, className = '', style = {} }) {
+export default function ({ as: Component = 'section', children, className = '', ...rest }) {
     return (
-        <Component className={twMerge('py-8 md:py-12 xl:py-16', className)} style={style}>
-            <div className='mx-auto px-4 sm:px-6 md:max-w-4xl md:px-4 lg:max-w-7xl xl:max-w-8xl 2xl:max-w-9xl lg:px-12'>
-                {children}
-            </div>
+        <Component className={twMerge('py-8 md:py-12 xl:py-16', className)} {...rest}>
+            {typeof Component === 'string' ? (
+                <div className='mx-auto px-4 sm:px-6 md:max-w-4xl md:px-4 lg:max-w-7xl xl:max-w-8xl 2xl:max-w-9xl lg:px-12'>
+                    {children}
+                </div>
+            ) : (
+                children
+            )}
         </Component>
     );
 }
