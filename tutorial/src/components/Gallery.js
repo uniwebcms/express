5c34c1435bc2b38705e0a1d0a6b7ab3cbc7b8a58
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2023-07-26 17:26:34 -0400

    last fix

--- tutorial/src/components/Gallery.js
+++ tutorial/src/components/Gallery.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Gallery, Item } from 'react-photoswipe-gallery';
 import 'photoswipe/dist/photoswipe.css';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import './index.css';
 
 export default function (props) {
     const { images = [], sizes = [] } = props;
+
+    const { siteConfig } = useDocusaurusContext();
+    const { baseUrl } = siteConfig;
 
     return (
         <div style={{}}>
@@ -20,32 +24,36 @@ export default function (props) {
                         width: '100%',
                         borderRadius: '8px'
                     }}>
-                    {images.map((image, index) => (
-                        <Item
-                            key={index}
-                            original={image}
-                            thumbnail={image}
-                            width={sizes[index]?.[0] || '800'}
-                            height={sizes[index]?.[1] || '600'}>
-                            {({ ref, open }) => (
-                                <img
-                                    ref={ref}
-                                    onClick={open}
-                                    style={{
-                                        cursor: 'pointer',
-                                        width: '180px',
-                                        height: '180px',
-                                        objectFit: 'contain',
-                                        background: 'white',
-                                        borderRadius: '8px',
-                                        boxShadow:
-                                            '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)'
-                                    }}
-                                    src={image}
-                                />
-                            )}
-                        </Item>
-                    ))}
+                    {images.map((image, index) => {
+                        const finalUrl = `${baseUrl}${image}`;
+
+                        return (
+                            <Item
+                                key={index}
+                                original={finalUrl}
+                                thumbnail={finalUrl}
+                                width={sizes[index]?.[0] || '800'}
+                                height={sizes[index]?.[1] || '600'}>
+                                {({ ref, open }) => (
+                                    <img
+                                        ref={ref}
+                                        onClick={open}
+                                        style={{
+                                            cursor: 'pointer',
+                                            width: '180px',
+                                            height: '180px',
+                                            objectFit: 'contain',
+                                            background: 'white',
+                                            borderRadius: '8px',
+                                            boxShadow:
+                                                '0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1)'
+                                        }}
+                                        src={finalUrl}
+                                    />
+                                )}
+                            </Item>
+                        );
+                    })}
                 </div>
             </Gallery>
         </div>
